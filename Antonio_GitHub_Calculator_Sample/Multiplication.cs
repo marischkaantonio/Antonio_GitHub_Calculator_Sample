dfeebce7788c3b0a947c7e6371cc82e8546568fd
rnzodjsus <rnzodjsus@gmail.com>
2026-02-11 20:26:15 +0800

    feat: create multiplication

---
+++ Antonio_GitHub_Calculator_Sample/Multiplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antonio_GitHub_Calculator_Sample
+{
+    public class Multiplication
+    {
+        public double multiply(double num1, double num2) {
+
+            return num1 * num2;
+
+        }   
+       
+    }
+}
