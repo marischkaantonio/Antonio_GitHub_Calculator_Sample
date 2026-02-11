cbb101b8abd54a824d95b50385fed5a09ed7abab
cabuhatprince-droid <cabuhatprince60@gmail.com>
2026-02-11 20:26:28 +0800

    feat : create divison

---
+++ Antonio_GitHub_Calculator_Sample/Division.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Antonio_GitHub_Calculator_Sample
+{
+    public class Division
+
+    { 
+       public double division(double num1, double num2)
+            { return num1 + num2; } 
+
+     
+
+    } 
+
+}
